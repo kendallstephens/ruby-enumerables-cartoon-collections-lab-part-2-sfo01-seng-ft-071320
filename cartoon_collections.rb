bba6f60214f9b5475bfb29f50a9b80aa4434d885
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-06 01:57:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |call| valid_calls.include? c
+  planeteer_calls.find do |call| valid_calls.include?(call)
   end
 
