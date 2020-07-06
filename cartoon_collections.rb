3c32a9260ea7f58b9dbc10a478a36395d9473b45
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-06 00:49:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.each do |planeteer_calls|
-    puts "#{planeteer_calls}.titleize !"
+    puts "#{planeteer_calls}.titleize "
   end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
